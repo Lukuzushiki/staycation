82044f4f54e45f6cbff248c854f03cbbee7e8253
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2025-07-25 22:25:34 +0700

    fix: fix withRouter used

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -1,138 +1,120 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import propTypes from "prop-types";
-
 import Button from "elements/Button";
 import { InputDate, InputNumber } from "elements/Form";
 
-class BookingForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        duration: 1,
-        date: {
-          startDate: new Date(),
-          endDate: new Date(),
-          key: "selection",
-        },
-      },
-    };
-  }
+const BookingForm = ({ ItemDetails, startBooking }) => {
+  const navigate = useNavigate();
 
-  updateData = (e) => {
-    this.setState({
-      ...this.state,
-      data: {
-        ...this.state.data,
-        [e.target.name]: e.target.value,
-      },
-    });
+  const [data, setData] = useState({
+    duration: 1,
+    date: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  });
+
+  useEffect(() => {
+    const startDate = new Date(data.date.startDate);
+    const endDate = new Date(data.date.endDate);
+    const countDuration = (endDate - startDate) / (1000 * 3600 * 24) + 1;
+
+    if (countDuration !== data.duration) {
+      setData((prev) => ({ ...prev, duration: countDuration }));
+    }
+  }, [data.date]);
+
+  useEffect(() => {
+    const startDate = new Date(data.date.startDate);
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + Number(data.duration) - 1);
+
+    if (data.date.endDate.toDateString() !== endDate.toDateString()) {
+      setData((prev) => ({
+        ...prev,
+        date: {
+          ...prev.date,
+          endDate,
+        },
+      }));
+    }
+  }, [data.duration]);
+
+  const updateData = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { data } = this.state;
-
-    if (prevState.data.date !== data.date) {
-      const startDate = new Date(data.date.startDate);
-      const endDate = new Date(data.date.endDate);
-      const countDuration = new Date(endDate - startDate).getDate();
-      this.setState({
-        data: {
-          ...this.state.data,
-          duration: countDuration,
-        },
-      });
-    }
-
-    if (prevState.data.duration !== data.duration) {
-      const startDate = new Date(data.date.startDate);
-      const endDate = new Date(
-        startDate.setDate(startDate.getDate() + +data.duration - 1)
-      );
-      this.setState({
-        ...this.state,
-        data: {
-          ...this.state.data,
-          date: {
-            ...this.state.data.date,
-            endDate: endDate,
-          },
-        },
-      });
-    }
-  }
-
-  startBooking = () => {
-    const { data } = this.state;
-    this.props.startBooking({
-      _id: this.props.ItemDetails._id,
+  const handleBooking = () => {
+    startBooking({
+      _id: ItemDetails._id,
       duration: data.duration,
       date: {
         startDate: data.date.startDate,
         endDate: data.date.endDate,
       },
     });
-    this.props.history.push("/checkout");
+    navigate("/checkout");
   };
-  render() {
-    const { data } = this.state;
-    const { ItemDetails, startBooking } = this.props;
-    return (
-      <div className="card bordered" style={{ padding: "60px 80px" }}>
-        <h4 className="mb-3">Start Booking</h4>
-        <h5 className="h2 text-teal mb-4">
-          ${ItemDetails.price}{" "}
-          <span className="text-gray-500 font-weight-light">
-            per {ItemDetails.unit}
-          </span>
-        </h5>
 
-        <label htmlFor="duration">How long you will stay?</label>
-        <InputNumber
-          max={30}
-          suffix={" night"}
-          isSuffixPlural
-          onChange={this.updateData}
-          name="duration"
-          value={data.duration}
-        />
+  return (
+    <div className="card bordered" style={{ padding: "60px 80px" }}>
+      <h4 className="mb-3">Start Booking</h4>
+      <h5 className="h2 text-teal mb-4">
+        ${ItemDetails.price}{" "}
+        <span className="text-gray-500 font-weight-light">
+          per {ItemDetails.unit}
+        </span>
+      </h5>
 
-        <label htmlFor="date">Pick a date</label>
-        <InputDate onChange={this.updateData} name="date" value={data.date} />
+      <label htmlFor="duration">How long you will stay?</label>
+      <InputNumber
+        max={30}
+        suffix=" night"
+        isSuffixPlural
+        onChange={updateData}
+        name="duration"
+        value={data.duration}
+      />
 
-        <h6
-          className="text-gray-500 font-weight-light"
-          style={{ marginBottom: 40 }}
-        >
-          You will pay{" "}
-          <span className="text-gray-900">
-            ${ItemDetails.price * data.duration} USD
-          </span>{" "}
-          per{" "}
-          <span className="text-gray-900">
-            {data.duration} {ItemDetails.unit}
-          </span>
-        </h6>
+      <label htmlFor="date">Pick a date</label>
+      <InputDate onChange={updateData} name="date" value={data.date} />
 
-        <Button
-          className="btn"
-          hasShadow
-          isPrimary
-          isBlock
-          onClick={this.startBooking}
-        >
-          Continue to Book
-        </Button>
-      </div>
-    );
-  }
-}
+      <h6
+        className="text-gray-500 font-weight-light"
+        style={{ marginBottom: 40 }}
+      >
+        You will pay{" "}
+        <span className="text-gray-900">
+          ${ItemDetails.price * data.duration} USD
+        </span>{" "}
+        for{" "}
+        <span className="text-gray-900">
+          {data.duration} {ItemDetails.unit}
+        </span>
+      </h6>
+
+      <Button
+        className="btn"
+        hasShadow
+        isPrimary
+        isBlock
+        onClick={handleBooking}
+      >
+        Continue to Book
+      </Button>
+    </div>
+  );
+};
 
 BookingForm.propTypes = {
   ItemDetails: propTypes.object,
   startBooking: propTypes.func,
 };
 
-export default withRouter(BookingForm);
+export default BookingForm;
